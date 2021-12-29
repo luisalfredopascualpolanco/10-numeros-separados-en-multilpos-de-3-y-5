c65b34be7810aad6d3796a7f5048c6832342a5af
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2021-12-29 10:35:56 -0800

    version1.0

---
+++ proyecto10.4/proyecto10.4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto10._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int posicion, numero;
+                int multiplo5 = 0;
+                int multiplo3 = 0;
+                int multiploAmbos = 0;
+                int ninguno = 0;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    posicion = i + 1;
+                    Console.Write("Dame el que va en la posicion #" + posicion + ": ");
+                    numero = int.Parse(Console.ReadLine());
+
+                    if (numero % 5 == 0 && numero % 3 == 0)
+                    {
+                        multiploAmbos = multiploAmbos + 1;
+                    }
+                    else if (numero % 3 == 0)
+                    {
+                        multiplo3 = multiplo3 + 1;
+                    }
+                    else if(numero % 5 ==0)
+                    {
+                        multiplo5 = multiplo5 + 1;
+                    }
+                    else
+                    {
+                        ninguno = ninguno + 1;
+                    }
+                }
+                Console.WriteLine("Hemos encontrado " + multiplo3 + " numeros multiplos de 3.");
+                Console.WriteLine("Hemos encontrado " + multiplo5 + " numeros multiplos de 5.");
+                Console.WriteLine("Hemos encontrado " + multiploAmbos + " numeros multiplos de ambos.");
+                Console.WriteLine("Hemos encontrado " + ninguno + " numeros que no son multiplos de ninguno.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ha introducido un valor invalido");               
+            }
+            Console.ReadKey();
+        }
+    }
+}
